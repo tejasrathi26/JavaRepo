8f2486fb9a41d4bd37efdcc3dea953c9481c0a66
tejas <tjsrth48@gmail.com>
2022-05-16 15:39:34 +0530

    second commit

--- src/test/RESTAPITest.java
+++ src/test/RESTAPITest.java
@@ -8,7 +8,18 @@ public class RESTAPITest{
     }
 
     @Test
+    void restApiApp2(){
+        System.out.println("AppiumApp2");
+    }
+
+    @Test
     void restApiCode(){
         System.out.println("IOSCode");
     }
+
+    @Test
+    void restApiCode2(){
+        System.out.println("IOSCode2");
+    }
+
 }
